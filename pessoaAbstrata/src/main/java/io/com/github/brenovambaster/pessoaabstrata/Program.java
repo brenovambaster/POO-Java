091619b3cfdd265f26c54811aaefb5dfde765c30
breno <brenovambaster5@gmail.com>
2022-10-19 13:40:39 -0300

    update projects

---
+++ pessoaAbstrata/src/main/java/io/com/github/brenovambaster/pessoaabstrata/Program.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package io.com.github.brenovambaster.pessoaabstrata;
+
+/**
+ *
+ * @author Breno Vambaster C. L
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
